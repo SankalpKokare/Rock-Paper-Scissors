6164c598d7d57aff4da6f7bf26668a331a0749dc
Sankalp Kokare <sankalpkokare21@gmail.com>
2023-07-21 19:35:43 +0530

    added button

--- script.js
+++ script.js
@@ -1,94 +1,129 @@
 
-function getComputerChoice()  // generate computer choice
+//set variable to buttons
+const rockButton = document.getElementById("rock");
+const PaperButton = document.getElementById("paper");
+const ScissorButton = document.getElementById("scissors");
+
+//set variable for text
+const gameScoreTxt = document.getElementById("gameScore");
+const winnerBoard = document.getElementById("winnerBoard");
+
+//set eventlistener to button 
+rockButton.addEventListener ("click" , setRock );
+
+PaperButton.addEventListener ("click" , setPaper );
+
+ScissorButton.addEventListener ("click" , setScissors );
+
+//get random value from computer
+function getComputerChoice()  
 {
     const choice = ["rock", "paper", "scissors"];
     const randomIndex = Math.floor((Math.random() * choice.length));
     return choice[randomIndex];
 }
 
-let userScore = 0;          
-let computerScore = 0;
+//set value from event listener to variable
 
-function score(x)  //count score
+let userValue = "";
+let computerValue = "";
 
-{
-
-    if (x == 1) {
-        ++userScore;
-    } else if (x == 2) {
-        ++computerScore
-    }
-
+function setRock () {
+    console.log("Rock button click");
+    userValue = "rock";
+    computerValue = getComputerChoice();
+    console.log(userValue);
+    console.log(computerValue);
+    game(userValue , computerValue);
 }
 
-function playRound(user, computer) 
+function setPaper () {
+    console.log("Paper button click");
+    userValue = "paper";
+    computerValue = getComputerChoice();
+    console.log(userValue);
+    console.log(computerValue);
+    game(userValue , computerValue);
+}
+
+function setScissors () {
+    console.log("Scissors button click");
+    userValue = "scissors";
+    computerValue = getComputerChoice();
+    console.log(userValue);
+    console.log(computerValue);
+    game(userValue , computerValue);
+}
+
+//score variable
+let userScore = 0;
+let computerScore = 0;
+let tieScore =  0;
+
+//game function 
+
+function game(user, computer) 
 
 {
 
     if (user == computer) {
+      
         console.log("Its a tie");
+        tieScore++;
+        gameCount();
+        
     }
     else if (user == "rock" && computer == "paper") {
         console.log("You Loose! Paper beats Rock");
-        score(2);
+        computerScore++;
+        gameCount();
 
     }
     else if (user == "paper" && computer == "rock") {
         console.log("You Win! Paper beats Rock");
-        score(1);
+        userScore++
+        gameCount();
 
     }
     else if (user == "scissors" && computer == "paper") {
         console.log("You Win! Scissors beats paper");
-        score(1);
+        userScore++;
+        gameCount();
 
     }
     else if (user == "paper" && computer == "scissors") {
         console.log("You Loose! Scissors beats paper");
-        score(2);
+        computerScore++;
+        gameCount();
 
     }
     else if (user == "rock" && computer == "scissors") {
         console.log("You Win! Rock beats scissors");
-        score(1);
+        userScore++;
+        gameCount();
 
     }
     else if (user == "scissors" && computer == "rock") {
         console.log("You loose! Rock beats scissors");
-        score(2);
+        computerScore++;
+        gameCount();
     }
     else {
         console.log("Wrong value entered");
     }
-
 }
 
+//result 
+function gameCount () {
+    console.log (computerScore , userScore, tieScore);
+    gameScoreTxt.textContent = "User Score " + userScore + " Computer score " + computerScore;
 
-
-let computerSelection = getComputerChoice();
-
-
-
-for (let i = 1; i <= 5; i++) {
-    let playerSelection = prompt(i + " out of 5: Enter Rock or Paper or Scissors");
-    playerSelection = playerSelection.toLowerCase();
-    playRound(playerSelection, computerSelection);
+    if ((computerScore+userScore+tieScore)==6) {
+        alert("alert");
+        userScore = 0;
+        computerScore = 0;
+        tieScore = 0;
+        gameScoreTxt.textContent = "User Score " + userScore + " Computer score " + computerScore;
+    }
+    
 }
-
-if (userScore > computerScore) // final score
- {
-    console.log("You Win!", userScore, computerScore);
-} else if (userScore < computerScore) {
-    console.log("You Loose!", userScore, computerScore);
-} else {
-    console.log("Its a tie ", userScore, computerScore);
-}
-
-
-
-
-
-
-
-
-
